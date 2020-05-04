78e111098e0cbec65dfb5cbd5b2ccc8bbf4e9e63
Branko Daskijevic <branko.dask@gmail.com>
2020-05-04 21:56:40 +0200

    adding some file structure

---
+++ src/components/layout/navbar/navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import styles from './navbar.module.css'
+
+export default function navbar() {
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={styles.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={styles.title}>
+            ExpenseTracker
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
