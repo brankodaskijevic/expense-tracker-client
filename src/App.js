3533bab8e5362b57805c125f9e5e3551c9ac4961
Branko Daskijevic <branko.dask@gmail.com>
2020-05-04 15:16:13 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Button from '@material-ui/core/Button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -51,6 +50,9 @@ function App() {
           </a>
         </span>
       </header>
+      <Button variant="contained" color="primary">
+        Test button
+      </Button>
     </div>
   );
 }
