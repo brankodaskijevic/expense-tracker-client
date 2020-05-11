2e87f6f6cc0af72bcaf283b03220ac266c66d34f
Branko Daskijevic <branko.dask@gmail.com>
2020-05-11 17:25:14 +0200

    some commit

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,35 +1,41 @@
-import React, { Fragment } from 'react'
-import { Link } from 'react-router-dom'
-import styles from './post.module.css'
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import styles from "./post.module.css";
 
 const Post = ({ post }) => {
+  const { description, likes, user } = post;
 
-  const {
-    description,
-    likes,
-    user,
-  } = post
-
-  const defaultImage = "https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png"
+  const defaultImage =
+    "https://cdn3.iconfinder.com/data/icons/avatars-round-flat/33/avat-01-512.png";
 
   return (
     <Fragment>
       <div className={styles.postContainer}>
         <div className={styles.leftPostSide}>
-          <img src={user.image ? user.image : defaultImage} alt="avatar" className={styles.profilePicture} />
-          <Link to="/user" className={styles.profileLink}>{user.username}</Link>
+          <img
+            src={user.image ? user.image : defaultImage}
+            alt="avatar"
+            className={styles.profilePicture}
+          />
+          <Link to="/user" className={styles.profileLink}>
+            {user.username}
+          </Link>
         </div>
         <div className={styles.rightPostSide}>
-          { description }
+          {description}
           <div className={styles.devider}></div>
           <div className={styles.likeElement}>
-            <input type="submit" value="Like" className={`${styles.btn} ${styles.btnPrimary} ${styles.lead}`} />
-            { likes.length }
+            <input
+              type="submit"
+              value="Like"
+              className={`${styles.btn} ${styles.btnPrimary} ${styles.lead}`}
+            />
+            {likes.length}
           </div>
         </div>
       </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
